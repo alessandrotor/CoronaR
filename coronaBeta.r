5ac2f20a5defa5d95387e7353bd2ec200225d4cb
epidemotraining <60882634+epidemotraining@users.noreply.github.com>
2020-02-26 19:01:21 +0100

    Dati aggiornati al 25 febbraio
    Semplice aggiornamento dei dati

--- coronaBeta.r
+++ coronaBeta.r
@@ -1,9 +1,9 @@
 #inserimento dati
-casi <- c(2102, 2590, 2831, 3235, 3893, 3697, 3151, 3401, 2657, 3073, 2484, 2022, 1820, 1998,1506, 1121, 2051, 1891, 1752, 399)
+casi <- c(2102, 2590, 2831, 3235, 3893, 3697, 3151, 3401, 2657, 3073, 2484, 2022, 1820, 1998,1506, 1121, 2051, 1891, 1752, 399, 894, 397, 650, 415, 518)
 severi <- c(268, 315, 186, 492, 431, 640, 962, 1280, 87, 298)
 #No idea 
 g_range <- range(0, casi)
 #disegno grafico
-plot(casi, type="o", col="red", ylim=g_range, xlab="giorni", ylab="Incidenza in 24h", main="Casi da laboratorio in Cina Febbraio", xlim = c(1,20))
+plot(casi, type="o", col="red", ylim=g_range, xlab="giorni", ylab="Incidenza in 24h", main="Casi da laboratorio in Cina Febbraio", xlim = c(1,25))
 #codice che potrebbe essere utile in futuro
 axis(1, at = seq(1, 10, by = 1), las=2)
